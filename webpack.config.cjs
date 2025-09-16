6f347722ef503c9c6a4e9219a414475611c35e86
Tatiana Lepestova <tatiana.lepestova@clickhelp.com>
2025-09-16 17:31:22 +0300

    Fix: deploy

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'docs'),
     filename: 'bundle.[contenthash].js',
     publicPath: '/',
   },
